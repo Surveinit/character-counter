a596796c920f40f257cd40c201fee4d4a1cf4ce5
Surve <surve8729@gmail.com>
2025-01-25 21:33:33 +0530

    Setup Notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,21 @@
 class NotesController < ApplicationController
   def index
+    @notes = Note.all
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to(root_path, notice: "Note saved successfully!")
+    else
+      render(:index, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content)
   end
 end
