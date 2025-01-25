ca6a1bc5665a215a0c7f29d9eb48a31aa108c0f3
Surve <surve8729@gmail.com>
2025-01-25 21:43:24 +0530

    Implement the character counter logic

--- app/javascript/controllers/character_count_controller.js
+++ app/javascript/controllers/character_count_controller.js
@@ -2,6 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="character-count"
 export default class extends Controller {
+  static targets = ["input", "counter"];
+  static values = { max: Number };
   connect() {
+  }
+
+  updateCounter() {
+    const remaining = this.maxValue - this.inputTarget.value.length;
+    this.counterTarget.textContent = '${remaining} characters remaining';
+
+    if (remaining < 0) {
+      this.counterTarget.classList.add("text-red-500");
+    } else {
+      this.counterTarget.classList.remove("text-red-500");
+    }
   }
 }
